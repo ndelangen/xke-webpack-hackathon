bba4fbb2924f466589684c697c9085d18341c614
Wietse Venema <wvenema@xebia.com>
2018-04-17 19:56:35 +0200

    Case sensitive filesystems want to play too

--- webpack.entries.config.js
+++ webpack.entries.config.js
@@ -1,52 +1,52 @@
-const path = require('path');
+const path = require("path");
 
-const webpack = require('webpack');
-const WildcardsEntryWebpackPlugin = require('./lib/entrypointsPlugin');
-const GeneratePagePlugin = require('./lib/generatePageplugin');
+const webpack = require("webpack");
+const WildcardsEntryWebpackPlugin = require("./lib/entrypointsPlugin");
+const GeneratePagePlugin = require("./lib/generatePagePlugin");
 
 function resolve(dir) {
   return path.join(__dirname, dir);
 }
 
 module.exports = {
-  entry: WildcardsEntryWebpackPlugin.entry('./in/**/*.example.js'),
+  entry: WildcardsEntryWebpackPlugin.entry("./in/**/*.example.js"),
   output: {
-    path: resolve('./out'),
-    filename: '[name].js',
-    library: 'example_[hash]',
-    publicPath: '/',
+    path: resolve("./out"),
+    filename: "[name].js",
+    library: "example_[hash]",
+    publicPath: "/"
   },
 
-  mode: 'development',
-  devtool: 'source-map',
+  mode: "development",
+  devtool: "source-map",
 
   module: {
     rules: [
       {
         test: /\.example.jsx?$/,
         exclude: /node_modules/,
-        use: [resolve('./lib/addHMRLoader.js')],
+        use: [resolve("./lib/addHMRLoader.js")]
       },
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
+        use: ["babel-loader"]
       },
       {
         test: /\.ejs$/,
-        loader: 'ejs-loader',
-      },
-    ],
+        loader: "ejs-loader"
+      }
+    ]
   },
   plugins: [
     new WildcardsEntryWebpackPlugin(),
     new webpack.DllReferencePlugin({
-      context: resolve('.'),
-      manifest: require('./out/dll/react-manifest.json'),
+      context: resolve("."),
+      manifest: require("./out/dll/react-manifest.json")
     }),
     new GeneratePagePlugin({
-      template: resolve('./templates/iframe.ejs'),
-      parser: 'ejs',
-    }),
-  ],
+      template: resolve("./templates/iframe.ejs"),
+      parser: "ejs"
+    })
+  ]
 };
